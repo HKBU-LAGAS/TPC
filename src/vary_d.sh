8bac1c68d436dc7e17e1980329b8a99e1bbbfc5c
Anry Yang <AnryYang@users.noreply.github.com>
2023-11-14 12:22:24 +0800

    Update vary_d.sh

--- src/vary_d.sh
+++ src/vary_d.sh
@@ -3,6 +3,6 @@ declare -A Gamma=(["Cora"]="10" ["CiteSeer"]="6" ["MovieLens"]="6" ["LastFM"]="7
 
 for d in {16,32,54,128,256}
 do
-	python3.8 proposed.py --data $1 --alpha ${Alpha[$1]} --gamma ${Gamma[$1]} --d $d  >> ../log/vary_d_$1.log
+	python3 proposed.py --data $1 --alpha ${Alpha[$1]} --gamma ${Gamma[$1]} --d $d  >> ../log/vary_d_$1.log
 	python3 eval.py --data $1 --algo TPC  >> ../log/vary_d_$1.log
 done
