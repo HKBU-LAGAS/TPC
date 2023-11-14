49cb4644f80abae721509aaff144163d887e6fb5
Anry Yang <AnryYang@users.noreply.github.com>
2023-11-14 12:22:09 +0800

    Update vary_alpha.sh

--- src/vary_alpha.sh
+++ src/vary_alpha.sh
@@ -3,6 +3,6 @@ declare -A Dim=(["Cora"]="128" ["CiteSeer"]="32" ["MovieLens"]="-1" ["LastFM"]="
 
 for a in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}
 do
-	python3.8 proposed.py --data $1  --alpha $a --gamma ${Gamma[$1]} --d ${Dim[$1]}  >> ../log/vary_alpha_$1.log
+	python3 proposed.py --data $1  --alpha $a --gamma ${Gamma[$1]} --d ${Dim[$1]}  >> ../log/vary_alpha_$1.log
 	python3 eval.py --data $1 --algo TPC  >> ../log/vary_alpha_$1.log
 done
