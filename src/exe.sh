b70420259c7d80e19242125654d1bc876c19ef16
Anry Yang <AnryYang@users.noreply.github.com>
2023-11-15 10:36:11 +0800

    Update exe.sh

--- src/exe.sh
+++ src/exe.sh
@@ -1,6 +1,6 @@
-declare -A Alpha=(["Cora"]="0.9" ["CiteSeer"]="0.6" ["MovieLens"]="0.6" ["LastFM"]="0.5" ["Google"]="0.9" ["Amazon"]="0.5")
-declare -A Gamma=(["Cora"]="10" ["CiteSeer"]="6" ["MovieLens"]="6" ["LastFM"]="7" ["Google"]="10" ["Amazon"]="0")
-declare -A Dim=(["Cora"]="128" ["CiteSeer"]="32" ["MovieLens"]="-1" ["LastFM"]="128" ["Google"]="32" ["Amazon"]="64")
+declare -A Alpha=(["Cora"]="0.9" ["CiteSeer"]="0.6" ["MovieLens"]="0.6" ["Google"]="0.9" ["Amazon"]="0.5")
+declare -A Gamma=(["Cora"]="10" ["CiteSeer"]="6" ["MovieLens"]="6" ["Google"]="10" ["Amazon"]="0")
+declare -A Dim=(["Cora"]="128" ["CiteSeer"]="32" ["MovieLens"]="-1" ["Google"]="32" ["Amazon"]="64")
 
 python3 proposed.py --data $1 --alpha ${Alpha[$1]} --gamma ${Gamma[$1]} --d ${Dim[$1]}
 python3 eval.py --data $1 --algo TPC
